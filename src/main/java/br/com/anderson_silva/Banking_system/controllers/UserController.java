b863bdd766b32bccf96284a9b0b77f87ae92e390
spider <spider@pop-os.localdomain>
2022-01-07 11:18:03 -0300

    struct initial

--- src/main/java/br/com/anderson_silva/Banking_system/controllers/UserController.java
+++ src/main/java/br/com/anderson_silva/Banking_system/controllers/UserController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("/")
     public  String hello(){
-        return  "Bank ok";
+        return  "Bank ok!";
     }
 }
